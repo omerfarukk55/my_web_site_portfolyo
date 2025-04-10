03136a9eda47d7faf3258f84db5d3772cdf56bb5
ömer faruk.k <94250423+omerfarukk55@users.noreply.github.com>
2025-04-10 17:36:39 +0300

    ES6+ destekli build sistemi eklendi

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,9 @@
 import gulp from 'gulp';
+import babel from 'gulp-babel';
 import cleanCSS from 'gulp-clean-css';
 import htmlmin from 'gulp-htmlmin';
 import imagemin from 'gulp-imagemin';
-import uglify from 'gulp-uglify';
+import terser from 'gulp-terser';
 
 // CSS dosyalarını minimize et
 export const minifyCSS = () => {
@@ -21,7 +22,10 @@ export const minifyHTML = () => {
 // JavaScript dosyalarını minimize et
 export const minifyJS = () => {
   return gulp.src('assets/js/*.js')
-    .pipe(uglify())
+    .pipe(babel({
+      presets: ['@babel/env']
+    }))
+    .pipe(terser())
     .pipe(gulp.dest('dist/assets/js'));
 };
 
